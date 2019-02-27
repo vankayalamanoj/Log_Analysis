f26e29ee0c901df2ca0ff98356ca355a699ea161
vankayalamanoj <manoj.vankayala@gmail.com>
2019-02-27 08:44:14 +0530

    small changes

--- log_analysis.py
+++ log_analysis.py
@@ -14,15 +14,15 @@ cur_object = connection.cursor()
 
 def pop_Auth():
     print '\nPopular Authors:\n'
-    myQuery = '''select name, sum(rqts) as tot_art_vws
+    myQry = '''select name, sum(rqts) as tot_art_vws
     from keysnrqst kyrst inner join articles art on art.slug = kyrst.ky
     inner join authors auth on auth.id = art.author
     group by name
     order by tot_art_vws Desc;'''
-    result = cur_object.execute(myQuery)
+    result = cur_object.execute(myQry)
     rows = cur_object.fetchall()
-    for value in rows:
-        print value[0], '--->', value[1], '\n'
+    for val in rows:
+        print val[0], '--->', val[1], '\n'
     print '------------------------------------------------------------'
 
 # Function to display the error rate greater than 1
@@ -30,31 +30,31 @@ def pop_Auth():
 
 def err_Rate():
     print '\nError Rate:\n'
-    myQuery = '''select total.date, round(fail.f_cnt*100.00/total.t_cnt, 2)
+    myQry = '''select total.date, round(fail.f_cnt*100.00/total.t_cnt, 2)
     as Rate
     from Fail_Count fail inner join Tot_Count total on fail.date = total.date
     where fail.f_cnt*100.00/total.t_cnt > 1 ; '''
-    result = cur_object.execute(myQuery)
+    result = cur_object.execute(myQry)
     rows = cur_object.fetchall()
-    for value in rows:
-        print value[0], '--->', value[1], '%', 'errors\n'
+    for val in rows:
+        print val[0], '--->', val[1], '%', 'errors\n'
 
 # Function To display the popular three articles with their views
 
 
 def pop_Art():
     print '\nPopular Articles:\n'
-    myQuery = '''select art.title, count(art.slug) as views
+    myQ = '''select art.title, count(art.slug) as views
     from articles art inner join log log on art.slug =
     replace(log.path, '/article/', '')
     where log.status = '200 OK' and log.path != '/'
     group by log.path, art.title
     order by count(log.path) Desc limit(3);'''
-    result = cur_object.execute(myQuery)
+    result = cur_object.execute(myQ)
     rows = cur_object.fetchall()
-    for value in rows:
-        print value[0], '--->', value[1], '\n'
-    print '------------------------------------------------------------'
+    for val in rows:
+        print val[0], '--->', val[1], '\n'
+    print '-------------------------------------------------------------'
 
 pop_Art()
 pop_Auth()
